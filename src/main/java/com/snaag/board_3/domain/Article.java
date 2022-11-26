61aa6e0e1326addec6b578e382577f1e6dc623da
sang-a-lee <sang-a.lee@navercorp.com>
2022-11-26 23:40:21 +0900

    domain 구현

---
+++ src/main/java/com/snaag/board_3/domain/Article.java
@@ -0,0 +1,40 @@
+package com.snaag.board_3.domain;
+
+public class Article {
+    private Long id;
+    private String name;
+    private String title;
+    private String context;
+
+    public Long getId() {
+        return id;
+    }
+
+    public void setId(Long id) {
+        this.id = id;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public String getTitle() {
+        return title;
+    }
+
+    public void setTitle(String title) {
+        this.title = title;
+    }
+
+    public String getContext() {
+        return context;
+    }
+
+    public void setContext(String context) {
+        this.context = context;
+    }
+}
